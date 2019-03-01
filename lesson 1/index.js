bc4560045ae7f00f4f1d0e08c8aee01c061d04c1
Anton <antiv1995@bk.ru>
2019-03-01 14:45:03 +0300

    ...

--- lesson 1/index.js
+++ lesson 1/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* 1 задание */
+
 var money = +prompt("Ваш бюджет на месяц?"),
     time = prompt("Введите дату в формате YYYY-MM-DD", ""),
     article = prompt("Введите обязательную статью расходов", ""),
@@ -16,7 +18,15 @@ var money = +prompt("Ваш бюджет на месяц?"),
         
     };   
     
+    /*2 задание*/
+    
     alert("Ваш бюджет на день: "+appData.budget/30);
+
+    var num = "33721", // создаем переменную "num"
+        multiplication = num[0]*num[1]*num[2]*num[3]*num[4]; // умножение цифр переменной
+        console.log(multiplication); // вывод в консоль результата
+    var cube = multiplication**3;
+        console.log(String(cube)[0]+String(cube)[1])
     
     
     

5f04d6afb87501c28d3c48dc2e178a59b31c3f0e
Anton <antiv1995@bk.ru>
2019-03-04 23:37:33 +0300

    последний коммит

--- lesson 3.1/index.js
+++ lesson 3.1/index.js
@@ -1,9 +1,37 @@
 "use strict";
 
 let str = "урок-3-был слишком легкий";
+    str = str.replace(/-/g, ' ');
+    str = str.slice(-6);
+    str = str.replace(str.slice(-2), "oо");
 
-let noBackspace = str.replace(/-/g, ' ');
-console.log(noBackspace);
+console.log(str);
 
-let easy = noBackspace.replace(/легкий/, ' ');
+let arr = [20, 33, 1, "Человек", 2, 3];
+var sum = 0;
+function cube(){    
+    for (let i = 0; i < arr.length; i++){
+        
+        if (isNaN(arr[i])) {              
+            arr.splice(i, i, i, i);
+        } else{
+            arr[i] = Math.pow(arr[i], 3);
+            sum += arr[i];
+
+        } 
+                   
+    }    
+}
+cube();
+
+console.log(Math.sqrt(sum));
+
+
+
+
+
+
+
+
+
 
---
+++ lesson 3.1/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let str = "урок-3-был слишком легкий";
+    str = str.replace(/-/g, ' ');
+    str = str.slice(-6);
+    
+
+console.log(str);

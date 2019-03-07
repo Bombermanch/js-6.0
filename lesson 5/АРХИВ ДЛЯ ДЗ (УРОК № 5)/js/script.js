9bf50b1ba63bf72462e55219a3aaec8ca03fcc71
Anton <antiv1995@bk.ru>
2019-03-08 00:58:40 +0300

    с изменением фона

--- lesson 5/АРХИВ ДЛЯ ДЗ (УРОК № 5)/js/script.js
+++ lesson 5/АРХИВ ДЛЯ ДЗ (УРОК № 5)/js/script.js
@@ -4,6 +4,7 @@ let menuItems = document.querySelectorAll('.menu-item'),
     header = document.querySelector('.title'),
     question = document.querySelector('#prompt')
     advertising = document.querySelector('.adv');
+    document.body.style.background = 'url(img/apple_true.jpg)';
 menu.insertBefore(menuItems[2], menuItems[1]);
 menuItemNew.classList.add('menu-item');
 menuItemNew.innerHTML = 'Пятый пункт';
@@ -11,5 +12,6 @@ menu.appendChild(menuItemNew);
 header.innerHTML = 'Мы продаём только подлинную технику Apple';
 advertising.remove();
 question.textContent = prompt("Ваше отношение к технике Apple", "");
+
 
     

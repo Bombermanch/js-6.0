4cb81d208db2851b4dd05d1579a1ace762cce06d
Anton <antiv1995@bk.ru>
2019-03-11 20:39:35 +0300

    выполненный 7 урок с основным заданием

---
+++ lesson 7.1/js/script.js
@@ -0,0 +1,22 @@
+
+function addTimer(){    
+    let date = new Date(),
+        hours = date.getHours(),
+        minutes = date.getMinutes(),
+        seconds = date.getSeconds(),
+        timer = document.querySelector('.timer');    
+
+    if(hours < 10){
+        hours = '0' + hours;
+    } else if (minutes < 10){
+        minutes = '0' + minutes;
+    } else if (seconds < 10){
+        seconds = '0' + seconds;
+    }
+
+    timer.textContent = hours + '\:' + minutes + '\:' + seconds;    
+
+}
+setInterval(addTimer, 1000);
+
+
--- lesson 7/js/script.js
+++ lesson 7/js/script.js
@@ -1,16 +1,19 @@
-window.addEventListener('DOMcontentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function(){
     'use strict';
+    
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
-
+        
     function hideTabContent(a){
-        for (i = a; i < tabContent.length; i++){
+        for (let i = a; i < tabContent.length; i++){
             tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
+            tabContent[i].classList.add('hide');            
         }
     }
     hideTabContent(1);
+    
+    
 
     function showTabContent(b){
         if (tabContent[b].classList.contains('hide')){
@@ -31,4 +34,6 @@ window.addEventListener('DOMcontentLoaded', function(){
             }
         }
     });
+    
 });
+    
